71efcc86d69e307d14dd19d6d4db32d80374cd9e
Valentin Nicolas <v.nicolas@MacBook-Pro-de-Valentin.local>
2017-06-08 18:33:38 +0200

    ng-if

--- app/js/controllers/matchController.js
+++ app/js/controllers/matchController.js
@@ -1,5 +1,6 @@
 angular.module('app')
-    .controller('MatchController', function($scope, UserService) {
+    .controller('MatchController', function($scope, UserService, CurrentUser) {
+      $scope.currentUser = CurrentUser.user();
       UserService.getAll().then(function(res) {
           $scope.users = res.data;
           console.log($scope.users);
